a2bdccbc1eeeca107e45fce82a65fa6a22425cce
facumruiz <facundoruiz105@gmail.com>
2022-09-02 10:39:04 -0300

    agrego actualizacion visual y funcional con excel

--- Form2CSV/main.py
+++ Form2CSV/main.py
@@ -3,59 +3,24 @@ from distutils.command.config import config
 from operator import length_hint
 from sre_parse import State
 import tkinter as tk
+import tkinter
+from tkinter import *
 
-from tkinter import DISABLED, NW, W, Tk, Label, Button, Entry, Frame, END
 from tkinter.font import NORMAL
 import pandas as pd
 
-root = tk.Tk()
+
+
+root = Tk()
 root.geometry("600x400")
 root.configure(bg="white")
 root.attributes("-fullscreen", True)
 root.bind("<F11>", lambda event: root.attributes("-fullscreen", not root.attributes("-fullscreen")))
 
+centro_num=5
 
-root.columnconfigure(0, weight=1)
-root.columnconfigure(1, weight=2)
-
-root.rowconfigure(1, weight=1)
-
-#seccion code
-seccion1 = tk.Label(root, text="Rectangle 1", bg="white", fg="white")
-seccion1.grid(column=0, row=0, columnspan=5, ipadx=20, ipady=100, sticky="NSEW")
-
-seccion2 = tk.Label(root, bg="white", fg="white")
-seccion2.grid(row=1, ipadx=250, sticky="NSEW")
-
-seccion3 = tk.Label(root, text="Rectangle 3", bg="white", fg="white")
-seccion3.grid(column=1, row=1, sticky="NSEW")
-
-seccion4 = tk.Label(root, text="Rectangle 3", bg="white", fg="white")
-seccion4.grid(column=2, row=1,ipadx=100, sticky="NSEW")
-
-def click1(event):
-	dato1.config(state=NORMAL)
-	dato1.delete(0, END)
-	
-def click2(event):
-	dato2.config(state=NORMAL)
-	dato2.delete(0, END)
-
-def click3(event):
-	dato3.config(state=NORMAL)
-	dato3.delete(0, END)
-
-def click4(event):
-	dato4.config(state=NORMAL)
-	dato4.delete(0, END)
-
-def click5(event):
-	dato5.config(state=NORMAL)
-	dato5.delete(0, END)
 
 d1, d2, d3, d4, d5 = [], [], [], [], []
-
-
 def agregar_datos():
 	global d1, d2, d3, d4, d5
 
@@ -77,54 +42,42 @@ def agregar_datos():
 		datos, columns=['Dato1', 'Dato2', 'Dato3', 'Dato4', 'Dato5'])
 	df.to_excel(nom_excel)  # index=False
 
+              
+for i in range(9):
+    root.grid_rowconfigure(i, weight=1)
+    for j in range(11):
+        root.grid_columnconfigure(j, weight=1)
+        
 
-nombre = Label(seccion3, text='Nombre', anchor=NW, width=10, bg='white', font=('Arial', 12, 'bold'))
-nombre.grid(column=0, row=0, ipady=0, padx=0, sticky="NW")
+Label(root, text=f'FOMRULARIO', anchor=N, width=10, bg='white', font=('Arial', 12, 'bold')).grid(row=1, column=5, sticky=EW)
 
-dato1 = Entry(seccion3, width=80,  bd=0, borderwidth=13, relief=tk.FLAT, font=('Arial', 12), bg='gray95')
-dato1.insert(0, 'Nombre')
-dato1.config(state=DISABLED)
-dato1.bind("<Button-1>", click1)
-dato1.grid(column=0, row=1, ipady=5, pady=10)
 
-apellido = Label(seccion3, text='Apellido', anchor=NW, width=10, bg='white', font=('Arial', 12, 'bold'))
-apellido.grid(column=0, row=2, pady=0, padx=0, sticky="NW")
+nombre = Label(root, text='Nombre', anchor=NW, width=10, bg='white', font=('Arial', 12, 'bold')).grid(row=2, column=5, sticky=NSEW)
+dato1 = Entry(root, width=80,  bd=0, borderwidth=17, relief=tk.FLAT, font=('Arial', 12), bg='gray95')
+dato1.grid(row=2, column=5, sticky=W)
 
-dato2 = Entry(seccion3, width=80, bd=0, borderwidth=13, relief=tk.FLAT, font=('Arial', 12), bg='gray95')
-dato2.insert(0, 'Apellido')
-dato2.config(state=DISABLED)
-dato2.bind("<Button-1>", click2)
-dato2.grid(column=0,row=3,ipady=5,pady=10)
+   
 
-edad = Label(seccion3, text='dato1', anchor=NW, width=10, bg='white', font=('Arial', 12, 'bold'))
-edad.grid(column=0, row=4, pady=0, padx=0, sticky="NW")
+apellido = Label(root, text='Apellido', anchor=NW, width=10, bg='white', font=('Arial', 12, 'bold')).grid(row=3, column=centro_num, sticky=NSEW)
+dato2 = Entry(root, width=80,  bd=0, borderwidth=17, relief=tk.FLAT, font=('Arial', 12), bg='gray95')
+dato2.grid(row=3, column=centro_num, sticky=W)
+        
+edad = Label(root, text='Edad', anchor=NW, width=10, bg='white', font=('Arial', 12, 'bold')).grid(row=4, column=centro_num, sticky=NSEW)
+dato3 = Entry(root, width=80,  bd=0, borderwidth=17, relief=tk.FLAT, font=('Arial', 12), bg='gray95')
+dato3.grid(row=4, column=centro_num, sticky=W)
+        
+        
+correo = Label(root, text='Correo', anchor=NW, width=10, bg='white', font=('Arial', 12, 'bold')).grid(row=5, column=centro_num, sticky=NSEW)
+dato4 = Entry(root, width=80,  bd=0, borderwidth=17, relief=tk.FLAT, font=('Arial', 12), bg='gray95')
+dato4.grid(row=5, column=centro_num, sticky=W)
+        
+        
+telefono = Label(root, text='Telefono', anchor=NW, width=10, bg='white', font=('Arial', 12, 'bold')).grid(row=6, column=centro_num, sticky=NSEW)
+dato5 = Entry(root, width=80,  bd=0, borderwidth=17, relief=tk.FLAT, font=('Arial', 12), bg='gray95')
+dato5.grid(row=6, column=centro_num, sticky=W)
 
-dato3 = Entry(seccion3, width=80, bd=0, borderwidth=13, relief=tk.FLAT, font=('Arial', 12), bg='gray95')
-dato3.insert(0, 'dato1')
-dato3.config(state=DISABLED)
-dato3.bind("<Button-1>", click3)
-dato3.grid(column=0, row=5, ipady=5, pady=10)
+agregar = Button(root, width=0,fg='white',  bd=0, font=('Arial', 15),text='Agregar', bg='dodger blue',command=agregar_datos).grid(column=5, row=7,pady=10, sticky="EW")
 
-correo = Label(seccion3, text='dato2', anchor=NW, width=10, bg='white', font=('Arial', 12, 'bold'))
-correo.grid(column=0, row=6, pady=0, padx=0, sticky="NW")
-
-dato4 = Entry(seccion3, width=80, bd=0, borderwidth=13, relief=tk.FLAT, font=('Arial', 12), bg='gray95')
-dato4.insert(0, 'dato2')
-dato4.config(state=DISABLED)
-dato4.bind("<Button-1>", click4)
-dato4.grid(column=0, row=7, ipady=5, pady=10)
-
-telefono = Label(seccion3, text='dato3', anchor=NW, width=10, bg='white', font=('Arial', 12, 'bold'))
-telefono.grid(column=0, row=8, pady=0, padx=0, sticky="NW")
-
-dato5 = Entry(seccion3, width=80, bd=0, borderwidth=13, relief=tk.FLAT, font=('Arial', 12), bg='gray95')
-dato5.insert(0, 'dato3')
-dato5.config(state=DISABLED)
-dato5.bind("<Button-1>", click5)
-dato5.grid(column=0, row=9, ipady=5, pady=10)
-
-agregar = Button(seccion3, width=10,fg='white',  bd=0, font=('Arial', 15),
-                 text='Agregar', bg='dodger blue', command=agregar_datos)
-agregar.grid(column=0, row=10,pady=10, sticky="NW")
 
 root.mainloop()
+
